7b60ec932ee5b5e2b73fb52301bb479723f5bbbd
Denny Sunny <iamdennysunny@gmail.com>
2026-03-24 17:32:27 +0530

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  supabase: {
-    url: "https://kjzrxaheeeoulvsbslys.supabase.co",
-    anonKey: "sb_publishable_ka4JcOp6vOGIc4kVmsxCeQ_TELVYyKT",
-  },
-};
